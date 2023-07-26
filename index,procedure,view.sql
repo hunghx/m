872ed40bb5461f50fa8fda3ed5a2d4ab5a401648
hunghx <120539776+hunghx@users.noreply.github.com>
2023-07-26 21:04:05 +0700

    Add files via upload

--- index,procedure,view.sql
+++ index,procedure,view.sql
@@ -126,3 +126,172 @@ call hocbonglon(150000)
 -- 14.Liệt kê những sinh viên nam của khoa Anh văn và khoa tin học, gồm các 
 -- thông tin: Mã sinh viên, Họ tên sinh viên, tên khoa, Phái.
 
+
+
+-- 31.Cho biết những nơi nào có hơn 2 sinh viên đang theo học tại trường.
+DELIMITER //
+
+CREATE PROCEDURE b31()
+BEGIN
+	SELECT noisinh, count(noisinh) from dmsv
+    group by noisinh
+    having count(noisinh) > 1;
+END //
+
+DELIMITER ;
+call b31();
+
+-- 32.Cho biết những môn nào có trên 3 sinh viên dự thi.
+DELIMITER //
+
+CREATE PROCEDURE b32()
+BEGIN
+	SELECT dmmh.TenMH, count(distinct kq.masv) from dmmh
+    join ketqua kq on dmmh.MaMH = kq.MaMH
+    group by kq.MaMH
+    having count(distinct kq.masv) > 3;
+END //
+
+DELIMITER ;
+call b32();
+
+-- 33.Cho biết những sinh viên thi lại trên 2 lần.
+DELIMITER //
+
+CREATE PROCEDURE b33()
+BEGIN
+	select dmsv.tensv, ketqua.Mamh,count(ketqua.lanthi) as `so lan thi` from ketqua 
+    join dmsv on dmsv.masv = ketqua.masv
+    group by ketqua.masv,ketqua.MaMH
+    having count(ketqua.lanthi) >= 2;
+END //
+
+DELIMITER ;
+call b33();
+
+
+-- 34.Cho biết những sinh viên nữ có điểm trung bình lần 1 trên 7.0
+DELIMITER //
+
+CREATE PROCEDURE b34()
+BEGIN
+	select dmsv.tensv, avg(ketqua.Diem) `diem TB`
+    from ketqua
+    join dmsv on dmsv.masv = ketqua.masv
+    where dmsv.phai = 'Nữ' and ketqua.lanthi = 1
+    group by ketqua.masv
+    having avg(ketqua.diem) > 7.0;
+END //
+
+DELIMITER ;
+call b34();
+-- 35.Cho biết danh sách các sinh viên rớt trên 2 môn ở lần thi 1.
+DELIMITER //
+create PROCEDURE task35()
+begin
+select MaSV, count(*) as 'so lan rot'
+from ketqua
+where Diem < 5 and lanthi = 1
+group by MaSv
+having count(*) > 1;
+end //
+DELIMITER ;
+call task35()
+
+-- 36.Cho biết danh sách những khoa có nhiều hơn 2 sinh viên nam
+DELIMITER //
+create PROCEDURE task36()
+begin
+select MaKhoa, count(*) as 'so luong sinh vien nam'
+from dmsv
+where phai = 'Nam'
+group by MaSv
+having count(*) > 2;
+end //
+DELIMITER ;
+call task36();
+-- 37.Cho biết những khoa có 2 sinh đạt học bổng từ 200.000 đến 300.000.
+select kh.* from dmsv sv
+join dmkhoa kh on sv.MaKhoa = kh.MaKhoa 
+group by kh.MaKhoa
+having count(case when sv.HocBong between 200000 and 300000 then sv.MaSV end) = 2;
+
+-- 38.Cho biết số lượng sinh viên đậu và số lượng sinh viên rớt của từng môn 
+-- trong lần thi 1.
+select mh.TenMH,count(case when kq.Diem < 4 then kq.MaSV end) 'rớt môn',count(case when kq.Diem >= 4 then kq.MaSV end) 'qua môn' from ketqua kq 
+join dmmh mh on mh.MaMH = kq.MaMH
+where kq.LanThi = 1 group by mh.TenMH;
+
+-- 39.Cho biết sinh viên nào có học bổng cao nhất.
+DELIMITER //
+create PROCEDURE sv39()
+begin
+select MaSV,HoSV,TenSV,HocBong from dmsv
+where hocbong = (select max(HocBong) from dmsv) 
+group by masv;
+end //
+DELIMITER ;
+call sv39();
+-- 40.Cho biết sinh viên nào có điểm thi lần 1 môn cơ sở dữ liệu cao nhất.
+DELIMITER //
+create PROCEDURE sv40()
+begin
+select dmsv.masv,dmsv.HoSV,dmsv.TenSV,kq.diem from dmsv 
+join ketqua kq on kq.masv =dmsv.masv
+join dmmh on dmmh.mamh = kq.mamh
+where dmmh.tenmh = 'Cơ Sở Dữ Liệu' and kq.lanthi =1
+order by kq.diem desc
+limit 1 ;
+end //
+DELIMITER ;
+call sv40();
+-- 41.Cho biết sinh viên khoa anh văn có tuổi lớn nhất.
+DELIMITER //
+create PROCEDURE sv41()
+begin
+select dv.MaKhoa,year(now())-year(NgaySinh) as tuoisv from dmsv dv
+where dv.Makhoa like 'AV'
+order by tuoisv desc
+limit 1;
+end //
+DELIMITER ;
+call sv41();
+-- 42.Cho biết khoa nào có đông sinh viên nhất.
+DELIMITER //
+create PROCEDURE sv42()
+begin
+select dv.MaKhoa,count(*) as sosv from dmsv dv
+group by dv.MaKhoa
+order by sosv desc
+limit 1;
+end //
+DELIMITER ;
+call sv42();
+drop procedure sv42;
+-- 43.Cho biết khoa nào có đông nữ nhất.
+-- 44.Cho biết môn nào có nhiều sinh viên rớt lần 1 nhiều nhất.
+-- 45.Cho biết sinh viên không học khoa anh văn có điểm thi môn phạm lớn hơn 
+-- điểm thi văncủa sinh viên học khoa anh văn.
+-- 46.Cho biết sinh viên có nơi sinh cùng với Hải.
+-- 47.Cho biết những sinh viên nào có học bổng lớn hơn tất cả học bổng của
+-- sinh viên thuộc khoa anh văn
+-- 48.Cho biết những sinh viên có học bổng lớn hơn bất kỳ học bổng của sinh viên 
+-- học khóa anh văn
+-- 49. ho biết sinh viên nào có điểm thi môn cơ sở dữ liệu lần 2 lớn hơn tất cả điểm 
+-- thi lần 1 môn cơ sở dữ liệu của những sinh viên khác.
+-- 50.Cho biết những sinh viên đạt điểm cao nhất trong từng môn.
+-- 51.Cho biết những khoa không có sinh viên học.
+-- 52.Cho biết sinh viên chưa thi môn cơ sở dữ liệu.
+-- 53.Cho biết sinh viên nào không thi lần 1 mà có dự thi lần 2.
+-- 54.Cho biết môn nào không có sinh viên khoa anh văn học.
+-- 55.Cho biết những sinh viên khoa anh văn chưa học môn văn phạm.
+-- 56.Cho biết những sinh viên không rớt môn nào.
+-- 57.Cho biết những sinh viên học khoa anh văn có học bổng và những sinh viên 
+-- chưa bao giờ rớt.
+-- 58.Cho biết khoa nào có đông sinh viên nhận học bổng nhất và khoa nào khoa 
+-- nào có ít sinh viên nhận học bổng nhất.
+-- 59.Cho biết 3 sinh viên có học nhiều môn nhất.
+-- 60.Cho biết những môn được tất cả các sinh viên theo học.
+-- 61.Cho biết những sinh viên học những môn giống sinh viên có mã số A02 học.
+-- 62.Cho biết những sinh viên học những môn bằng đúng những môn mà sinh 
+-- viên A02 học
